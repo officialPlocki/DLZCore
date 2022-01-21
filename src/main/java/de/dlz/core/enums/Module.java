903f5d58a012f59662f4b80a7a45f431d3e21338
ViaEnder <admin@viaender.de>
2022-01-21 13:19:11 +0100

    First Parts for SupportChat
    Signed-off-by: ViaEnder <admin@viaender.de>

--- src/main/java/de/dlz/core/enums/Module.java
+++ src/main/java/de/dlz/core/enums/Module.java
@@ -7,7 +7,8 @@ import org.bukkit.ChatColor;
 public enum Module {
   SKILL(ChatColor.DARK_GRAY + "[" + ChatColor.GOLD + "SKILL" + ChatColor.DARK_GRAY + "]"),
   CORE,
-  ECONOMY(ChatColor.DARK_GRAY + "[" + ChatColor.GOLD + "Economy" + ChatColor.DARK_GRAY + "]");
+  ECONOMY(ChatColor.DARK_GRAY + "[" + ChatColor.GOLD + "Economy" + ChatColor.DARK_GRAY + "]"),
+  ESSENTIALS(ChatColor.DARK_GRAY + "[" + ChatColor.GOLD + "Essentials" + ChatColor.DARK_GRAY + "]");
   private String prefix;
 
   Module() {
